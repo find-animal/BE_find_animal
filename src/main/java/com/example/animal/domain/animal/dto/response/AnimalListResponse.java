d5fd2d7b94c620054dc8e92459df9c82a8186790
unso99 <unso99@naver.com>
2024-05-30 19:58:16 +0900

    feat: 유기동물 리스트 조회 시 중성화 여부 추가

--- src/main/java/com/example/animal/domain/animal/dto/response/AnimalListResponse.java
+++ src/main/java/com/example/animal/domain/animal/dto/response/AnimalListResponse.java
@@ -1,7 +1,6 @@
 package com.example.animal.domain.animal.dto.response;
 
 import com.example.animal.domain.animal.entity.Animal;
-import java.time.LocalDate;
 import java.util.Objects;
 import lombok.Builder;
 
@@ -10,24 +9,20 @@ public record AnimalListResponse(
     Long id,
     String fileName,
     String animalBreed,
-    String happenPlace,
-    LocalDate happenDt,
     String age,
     String sex,
-    String weight,
-    LocalDate noticeSdt,
-    LocalDate noticeEdt
+    String neuterYn,
+    String noticeSdt,
+    String noticeEdt
 ) {
 
   public AnimalListResponse {
     Objects.requireNonNull(id, "id must not be null");
     Objects.requireNonNull(fileName, "fileName must not be null");
     Objects.requireNonNull(animalBreed, "animalBreed must not be null");
-    Objects.requireNonNull(happenPlace, "happenPlace must not be null");
-    Objects.requireNonNull(happenDt, "happenDt must not be null");
     Objects.requireNonNull(age, "age must not be null");
     Objects.requireNonNull(sex, "sex must not be null");
-    Objects.requireNonNull(weight, "weight must not be null");
+    Objects.requireNonNull(neuterYn, "neuterYn must not be null");
     Objects.requireNonNull(noticeSdt, "noticeSdt must not be null");
     Objects.requireNonNull(noticeEdt, "noticeEdt must not be null");
   }
@@ -37,13 +32,11 @@ public record AnimalListResponse(
         .id(animal.getId())
         .fileName(animal.getFileName())
         .animalBreed(animal.getAnimalBreed())
-        .happenPlace(animal.getHappenPlace())
-        .happenDt(animal.getHappenDt())
         .age(animal.getAge())
         .sex(animal.getSexCd().label)
-        .weight(animal.getWeight())
-        .noticeSdt(animal.getNoticeSdt())
-        .noticeEdt(animal.getNoticeEdt())
+        .neuterYn(animal.getNeuterYn().label)
+        .noticeSdt(animal.getNoticeSdt().toString())
+        .noticeEdt(animal.getNoticeEdt().toString())
         .build();
   }
 }
