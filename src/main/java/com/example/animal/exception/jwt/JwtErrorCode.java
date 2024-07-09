6ee3c7d42c4c65919c8f5011bbc8a1cffa1fa130
unso99 <unso99@naver.com>
2024-07-09 21:09:36 +0900

    update: jwt 에러 메세지 수정

--- src/main/java/com/example/animal/exception/jwt/JwtErrorCode.java
+++ src/main/java/com/example/animal/exception/jwt/JwtErrorCode.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum JwtErrorCode implements ErrorCode {
-  INVALID_TOKEN(3000,401,"Invalid JWT Token"),
-  EXPIRED_TOKEN(3001,401,"Expired JWT Token"),
+  INVALID_TOKEN(30000,401,"유효하지 않은 토큰 정보입니다."),
+  EXPIRED_TOKEN(30000,401,"로그인 정보가 만료되었습니다."),
   CLAIMS_IS_EMPTY(3002,400,"JWT claims string is empty"),
   ;
   private final int code;
